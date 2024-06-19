38ba3d7e53694d7e40472fda547889fa90991761
Will Atkinson <willreubena@gmail.com>
2024-06-19 15:07:52 +0100

    recent updates

--- scripts.js
+++ scripts.js
@@ -7,23 +7,33 @@ fetch("projects.json").then(function (res) {
 
     data.projects.forEach(function (project) {
         section3.innerHTML += `
-        <div class="projects">
-            <div class="project-info">
-                <div>
+        <div class="projects-outer">
+            <div class="project-container">
+                <div class="project-info">
                     <h2>${project.name}</h2>
                     <p>${project.desc}</p>
                 </div>
                 <div class="project-links">
-                    <a href="${project.plink}">Project link</a>
-                    <a href="${project.github}">Github</a>
+                    <a href="${project.plink}">Live <i class="fa-solid fa-link"></i></a>
+                    <a href="${project.github}"> Github <i class="fa-brands fa-square-github"></i></a>
                 </div>
                 
             </div>
-            <div class="placeholder-image">
-                <img src=""${project.img}/>
+            <div>
+            </div>
+            <div class="image-container">
+                <img class="project-image" src="${project.img}"/>
             </div>
         </div>
         `
     })
 
+    section3.innerHTML += `
+    <div class="arrow-4">
+        <a><i class="fa-solid fa-chevron-down"></i></a>
+    </div>
+    `
+    
 })
+
+// Reduce opacity when
